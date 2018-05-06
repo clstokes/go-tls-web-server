f98d6a63f8124053d5cb64d9deb8087e30867828
Cameron Stokes <cameron@cameronstokes.com>
2018-05-06 16:35:31 -0700

    move args parsing

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"github.com/armon/go-metrics"
 	"html"
 	"log"
 	"math/rand"
@@ -14,31 +15,35 @@ const (
 	ResponseOk = "{\"status\":\"ok\"}"
 )
 
+type ServerOpts struct {
+	listenAddr       string
+	clientCert       string
+	clientKey        string
+	maxCrashDuration int
+}
+
 func main() {
 	os.Exit(realMain())
 }
 
 func realMain() int {
-	listenAddr := flag.String("listen-address", ":8080", "address to listen on")
-	clientCert := flag.String("client-cert", "", "path to the CA certificate")
-	clientKey := flag.String("client-key", "", "path to the CA private key")
-	maxCrashDuration := flag.Int("crash", 0, "maximum duration to wait before crashing")
-	flag.Parse()
+	server := &ServerOpts{}
+	parseArgs(server)
 
-	if *maxCrashDuration != 0 {
-		setupCrashRoutine(*maxCrashDuration)
+	if server.maxCrashDuration != 0 {
+		setupCrashRoutine(server.maxCrashDuration)
 	}
 
-	log.Printf("Listening on [%v]...\n", *listenAddr)
+	log.Printf("Listening on [%v]...\n", server.listenAddr)
 
 	http.HandleFunc("/", handleRequest)
 	http.HandleFunc("/healthz", handleHealthzRequest)
 
 	var err error
-	if *clientCert != "" && *clientKey != "" {
-		err = http.ListenAndServeTLS(*listenAddr, *clientCert, *clientKey, nil)
+	if server.clientCert != "" && server.clientKey != "" {
+		err = http.ListenAndServeTLS(server.listenAddr, server.clientCert, server.clientKey, nil)
 	} else {
-		err = http.ListenAndServe(*listenAddr, nil)
+		err = http.ListenAndServe(server.listenAddr, nil)
 	}
 
 	if err != nil {
@@ -63,9 +68,17 @@ func setupCrashRoutine(maxCrashDuration int) {
 	rand.Seed(time.Now().Unix())
 	crashDuration := rand.Intn(maxCrashDuration)
 
-	log.Printf("Crashing in [%v] seconds", crashDuration)
+	log.Printf("Crashing in [%v] seconds...", crashDuration)
 	go func() {
 		time.Sleep(time.Duration(crashDuration) * time.Second)
 		log.Fatal("Crashing...")
 	}()
+}
+
+func parseArgs(server *ServerOpts) {
+	flag.StringVar(&server.listenAddr, "listen-address", ":8080", "address to listen on")
+	flag.StringVar(&server.clientCert, "client-cert", "", "path to the CA certificate")
+	flag.StringVar(&server.clientKey, "client-key", "", "path to the CA private key")
+	flag.IntVar(&server.maxCrashDuration, "crash", 0, "maximum duration to wait before crashing")
+	flag.Parse()
 }
